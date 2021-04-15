0bce5c88ce2236de1bd145ee879044b9a9ea00d3
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-04-15 22:58:37 +0200

    Update main.py
    add circle testing

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import sys
 
 sys.path.append(".")
 
-from pattern import Checker
+from pattern import Checker, Circle
 
 checker = Checker(600,30)
 checker.draw()
 checker.show()
+
+c = Circle(1024, 200, (512, 256))
+c.draw()
+c.show()
