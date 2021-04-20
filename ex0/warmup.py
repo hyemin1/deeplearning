3406df8760ae255d8bc3bdfe327db5ee9a1f5bed
pritomBrainStation <50450569+pritomBrainStation@users.noreply.github.com>
2021-04-20 19:50:35 +0600

    Up to shuffling  the images implemented.

--- ex0/warmup.py
+++ ex0/warmup.py
@@ -3,6 +3,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import random as rn
 
 image_List = os.listdir('/Users/rupak/Documents/Deep Learning Exersises/exercise0_material/src_to_implement/data/exercise_data')
 allImagesList = np.array([file for file in image_List if file.endswith('.npy')])
@@ -15,14 +16,18 @@ class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5:
 
 total = range(0,len(allImagesList))
 batch_size = 11
+shuffle = True
 
 if ((len(allImagesList) % batch_size) == 0):
     tempBatchNumber = int(len(allImagesList)/batch_size)
-    tempImagesList  = allImagesList.reshape(batch_size, tempBatchNumber)
+    tempImagesList  = allImagesList.reshape(tempBatchNumber,batch_size)
 else:
-    tempBatchNumber = math.floor(len(allImagesList)/batch_size)
-    remainder       = (len(allImagesList) % batch_size)
-    itemsToAdd      = batch_size - remainder
-    elementCount    = len(allImagesList) + itemsToAdd
-    tempImageList   = allImagesList[0:itemsToAdd]
-    print (np.concatenate(np.array(allImagesList),np.array(tempImageList)))
+    tempBatchNumber     = math.floor(len(allImagesList)/batch_size)
+    remainder           = (len(allImagesList) % batch_size)
+    itemsToAdd          = batch_size - remainder
+    additionalImageList = np.array(allImagesList[0:itemsToAdd])
+    tempImagesList      = np.concatenate((allImagesList, additionalImageList))
+    tempImagesList      = tempImagesList.reshape(tempBatchNumber + 1, batch_size)
+
+if shuffle:
+    tempImagesList = np.random.permutation(tempImagesList)
