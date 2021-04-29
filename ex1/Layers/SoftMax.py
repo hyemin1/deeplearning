5fc7ec9f6938df87886c8c17082f00a175062ef3
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-04-29 10:45:07 +0200

    forward corrected

--- ex1/Layers/SoftMax.py
+++ ex1/Layers/SoftMax.py
@@ -5,3 +5,4 @@ class SoftMax(BayesLayer):
         super().__init__()
     def forward(self,input_tensor):
         self.output_tresor = np.exp(input_tensor-max(input_tensor))/(np.sum(np.exp(input_tensor)))
+        return self.output_tensor
