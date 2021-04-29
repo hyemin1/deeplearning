2827398f420bcef394ea228a6520c911df752cd8
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-04-29 10:43:51 +0200

    forward corrected

--- ex1/Layers/FullyConnected.py
+++ ex1/Layers/FullyConnected.py
@@ -9,19 +9,24 @@ class FullyConnected(BayesLayer):
         self.weights= np.random.uniform(0,1,(self.output_size)*(self.input_size+1))
         self.weights=np.reshape((self.ouput_size,self.input_size+1))
         self.input_tensor=np.empty((1,1))
-
+        self.batch_size=0
+        self.gradient_weights=0
+        
     #need to define optimizer
 
     def forward(self,input_tensor):
-        self.input_tensor=input_tensor
+        self.batch_size=len(input_tensor)
+        input_tensor=np.flatten(input_tensor)
+        addition_ones  = np.ones((1,self.input_size))
+        self.input_tensor=np.concatenate(input_tensor,addition_ones)
+        self.input_tensor=np.reshape(self.input_tensor,(self.batch_size+1,self.input_size))
         self.output_tensor= np.matmul(self.weights,self.input_tensor)
-
         output=np.copy(self.output_tensor)
         return output
     def backward(self,error_tensor):
         self.prev_error = np.matmul(self.weights.T,error_tensor)
         prev = np.copy(self.prev_eror)
-        #have to add L, but don't understand how to compute
+        #probably wrong
         self.gradient_weights = (np.matmul(self.weights*self.input_tensor,self.input_tensor.T))
         self.weights = self.calculate_update(self.weights,self.gradient_weights)
         return prev
