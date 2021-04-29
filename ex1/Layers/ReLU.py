f49107738f714123605e0d1a9f9114e648b8f1f8
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-04-29 10:44:32 +0200

    forward corrected

--- ex1/Layers/ReLU.py
+++ ex1/Layers/ReLU.py
@@ -3,13 +3,15 @@ import numpy as np
 class ReLU(BayesLayer):
     def __init__(self):
         super().__init__()
-
+        self.batch_size=0
+        self.input_size=0
     def forward(self,input_tensor):
-        row = len(input_tensor)
-        col = len(input_tensor[0])
+        self.batch_size = len(input_tensor)
+        self.input_size = len(input_tensor[0])
         self.input_tensor = input_tensor.flatten()
-        self.ouput_tensor = np.array(max(0,x) for x in self.input_tensor)
-        self.ouput_tensor=np.reshape((row,col))
+
+        self.ouput_tensor = np.maximum(0,self.input_tensor)
+        self.ouput_tensor=np.reshape((self.batch_size,self.input_size))
         output=np.copy(self.ouput_tensor)
         return output
     def backward(self,error_tensor):
