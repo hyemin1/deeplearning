4698292872c3d1752ad28037263bf8a9d4d7a695
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-01 00:22:33 +0200

    changed calculate_update

--- ex1/Optimization/Optimizers.py
+++ ex1/Optimization/Optimizers.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-class Sgd:
+class Sgd():
     def __init__(self,learning_rate):
         self.learning_rate=learning_rate
 
     def calculate_update(self,weight_tensor,gradient_tensor):
         #updated weight = weight - learningrate * gradient(weight)
-        self.updated_weight = weight_tensor- self.learning_rate*np.gradient(gradient_tensor,weight_tensor)
+
+        #np.gradient(gradient_tensor,weight_tensor)
+        print(self.learning_rate)
+        print(gradient_tensor)
+        self.updated_weight = weight_tensor - (self.learning_rate)*(gradient_tensor)
+
         return self.updated_weight
