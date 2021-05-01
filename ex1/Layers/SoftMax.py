f6969db975489f56885007e92c1b6a8a373556a5
pritomBrainStation <50450569+pritomBrainStation@users.noreply.github.com>
2021-05-01 18:29:41 +0600

    Softmax implemented without passing some of the test cases.

--- ex1/Layers/SoftMax.py
+++ ex1/Layers/SoftMax.py
@@ -1,8 +1,20 @@
-from Bayes import BayesLayer
+from Base import BayesLayer
 import numpy as np
+
 class SoftMax(BayesLayer):
     def __init__(self):
         super().__init__()
+
     def forward(self,input_tensor):
-        self.output_tresor = np.exp(input_tensor-max(input_tensor))/(np.sum(np.exp(input_tensor)))
+        reduced_logit = input_tensor - np.amax(input_tensor)
+        exponential_of_current_logit = np.exp(reduced_logit)
+        exponential_of_all_logit = np.exp(input_tensor)
+        sum_of_all_exponential_logit = np.sum(exponential_of_all_logit)
+        self.output_tensor = exponential_of_current_logit/sum_of_all_exponential_logit
         return self.output_tensor
+
+    def backward(self, error_tensor):
+        sum_of_multiplication = np.sum(error_tensor * self.output_tensor)
+        subtraction_from_current_tensor = error_tensor - sum_of_multiplication
+        error_tensor_for_previous_layer = np.matmul(self.weights, subtraction_from_current_tensor)
+        return error_tensor_for_previous_layer
