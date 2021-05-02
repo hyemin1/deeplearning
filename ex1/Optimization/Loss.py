0beef0ef9eb5b33deac533c23990245e23dafe97
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-02 09:58:25 +0200

    passed all cross entropy unittests

--- ex1/Optimization/Loss.py
+++ ex1/Optimization/Loss.py
@@ -1,10 +1,20 @@
-from Bayes import BayesLayer
+
+from SoftMax import SoftMax as sft
 import numpy as np
-class CrossEntropyLoss(BayesLayer):
+class CrossEntropyLoss():
     def __init__(self):
         super().__init__()
-
+        self.input_size=0
+        self.soft =sft()
     def forward(self,input_tensor,label_tensor):
-        self.loss = np.sum(-(np.log(yk+np.finfo.eps) for yk in label_tensor if yk==1))
-        loss=np.copy(self.loss)
-        return loss
+        self.prob_label =input_tensor
+
+        #compute loss
+        loss = self.prob_label[np.arange(len(self.prob_label)),np.argmax(label_tensor,axis=1)]
+        loss += np.finfo(float).eps
+        self.loss = (-(np.log(loss))).sum()
+
+        return self.loss
+    def backward(self,label_tensor):
+        self.prev_error = -(np.divide(label_tensor,self.prob_label))
+        return self.prev_error
