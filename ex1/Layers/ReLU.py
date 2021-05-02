ab318bbca72066642e24a7e774efc3ae598811b4
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-02 17:17:46 +0200

    passed all unit tests(but may need corrections)

--- ex1/Layers/ReLU.py
+++ ex1/Layers/ReLU.py
@@ -1,70 +1,50 @@
 from Bayes import BayesLayer
-from Optimizers import Sgd as opt
 import numpy as np
-class FullyConnected(BayesLayer):
-    def __init__(self,input_size,output_size):
-        BayesLayer.__init__(self)
-        self.trainable=True
-        self.input_size=input_size
-        self.output_size=output_size
-        #self.weights= np.random.uniform(0,1,(self.input_size+1)*(self.output_size))
-        #self.weights=np.reshape(self.weights,(self.input_size+1,self.output_size))
-        self.input_tensor=np.empty((1,1))
+
+class ReLU(BayesLayer):
+    def __init__(self):
+        super().__init__()
         self.batch_size=0
-        self.gradient_weights=[0]
-
-        #optimizer
-        self._optimizer=opt(1)
-
-    @property
-    def optimizer(self):
-        return self._optimizer
-
-    @optimizer.setter
-    def optimizer(self,learning_rate):
-        self._optimizer = opt(learning_rate)
-
+        self.input_size=0
     def forward(self,input_tensor):
-        self.batch_size=len(input_tensor)
+        #re-assign batch_size and input_size
+        self.batch_size = len(input_tensor)
+        self.input_size = len(input_tensor[0])
 
-        #re-define input_tensor: add a col
-        self.input_tensor = np.ones((self.batch_size,self.input_size+1))
-        self.input_tensor[:,:-1]=input_tensor
-        #print(len(self.input_tensor))
-        #print(self.input_tensor)
-        #re-define weight
-        self.weights = np.random.uniform(0,1,((self.input_size+1)*self.output_size))
-        self.weights = np.reshape(self.weights,(self.input_size+1,self.output_size))
+        #make input_tensor 1D
+        self.input_tensor = input_tensor.flatten()
 
-        self.output_tensor= np.matmul(self.input_tensor,self.weights)
-        output=np.copy(self.output_tensor)
+        #apply ReLU function
+        self.ouput_tensor = np.maximum(0,self.input_tensor)
+
+        #reshape
+        print(self.ouput_tensor.shape)
+        self.ouput_tensor=np.reshape(self.ouput_tensor,(self.batch_size,self.input_size))
+        self.input_tensor = np.reshape(self.input_tensor,(self.batch_size,self.input_size))
+        output=np.copy(self.ouput_tensor)
         return output
-    #@property
-    #def gradient_weights(self):
-    #    return self.gradient_weights
     def backward(self,error_tensor):
-        #remove the column with 1s of weights
-        #self.weights=np.delete(self.weights,-1,1)
-        #Error tensor from the previous layer
-        # delete the last row of weights
-        self.weights = np.delete(self.weights, len(self.weights) - 1, 0)
-        #previous error tensor
-        self.prev_error = np.matmul(error_tensor,self.weights.T)
-        prev = np.copy(self.prev_error)
+        #get row, col for reshaping later
+        row = len(error_tensor)
+        col = len(error_tensor[0])
 
-        #remove the row with 1s of input tensor
-        self.input_tensor = np.delete(self.input_tensor,len(self.input_tensor[0])-1,1)
+        self.input_tensor=self.input_tensor.flatten()
+        #make error_tensor 1D
+        error_tensor=error_tensor.flatten()
+        #initialize prev_error with 0s
+        self.prev_error=[0]*len(error_tensor)
+        #apply ReLU function for backward computation
+        for i in range(len(self.input_tensor)):
+            if (self.input_tensor[i]>0):
+                self.prev_error[i]=error_tensor[i]
+        #reshape previous error tensor
+        self.prev_error=np.reshape(self.prev_error,(row,col))
+        #reshape current error tensor
+        error_tensor = np.reshape(error_tensor,(row,col))
+        #reshape input tensor
+        self.input_tensor=np.reshape(self.input_tensor,(self.batch_size,self.input_size))
 
-
-
-        #compute gradient w.r.t weight
+        #compute gradient w.r.t weights
         self.gradient_weights = np.matmul(self.input_tensor.T,error_tensor)
 
-        self.weights = self.calculate_update(self.weights,self.gradient_weights)
-        return prev
-
-    def calculate_update(self,weight_tensor,gradient_tensor):
-        #self._optimizer = opt(1)
-        #self._optimizer(1)
-        updated_weight = self._optimizer.calculate_update(weight_tensor,gradient_tensor)
-        return updated_weight
+        return self.prev_error
