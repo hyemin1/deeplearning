d4eaf23abb68f058268d722cfaf0662f494bb652
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-03 17:00:40 +0200

    problem in backpropagation

--- ex1/Layers/FullyConnected.py
+++ ex1/Layers/FullyConnected.py
@@ -1,4 +1,3 @@
-
 from Bayes import BayesLayer
 from Optimizers import Sgd as opt
 import numpy as np
@@ -12,19 +11,23 @@ class FullyConnected(BayesLayer):
         #self.weights=np.reshape(self.weights,(self.input_size+1,self.output_size))
         self.input_tensor=np.empty((1,1))
         self.batch_size=0
-        self.gradient_weights=[0]
+        #self.gradient_weights=[0]
 
         #optimizer
-        self._optimizer=opt(1)
+        self._optimizer=None
 
     @property
     def optimizer(self):
         return self._optimizer
 
     @optimizer.setter
-    def optimizer(self,learning_rate):
-        self._optimizer = opt(learning_rate)
-
+    def optimizer(self,sgd):
+        self._optimizer = sgd
+        print("here")
+        #print(learning_rate)
+    @property
+    def gradient_weights(self):
+        return self.gradient_w
     def forward(self,input_tensor):
         self.batch_size=len(input_tensor)
 
@@ -47,27 +50,29 @@ class FullyConnected(BayesLayer):
         #remove the column with 1s of weights
         #self.weights=np.delete(self.weights,-1,1)
         #Error tensor from the previous layer
-        # delete the last row of weights
+        # delete the last row(bias) of weights
         self.weights = np.delete(self.weights, len(self.weights) - 1, 0)
         #previous error tensor
         self.prev_error = np.matmul(error_tensor,self.weights.T)
         prev = np.copy(self.prev_error)
 
-        #remove the row with 1s of input tensor
+        self.curr_erro=error_tensor
+        #remove the col with 1s(bias) of input tensor
         self.input_tensor = np.delete(self.input_tensor,len(self.input_tensor[0])-1,1)
-
-
-
+        print(self.input_tensor.shape)
+        print(self.weights.shape)
+        print(error_tensor.shape)
         #compute gradient w.r.t weight
-        self.gradient_weights = np.matmul(self.input_tensor.T,error_tensor)
-
-        self.weights = self.calculate_update(self.weights,self.gradient_weights)
+        self.gradient_w = np.matmul(self.input_tensor.T,error_tensor)
+        #self.gradient_weights = self.calculate_update(self.weights, np.matmul(self.input_tensor.T,error_tensor))
+        #self.weights = self.calculate_update(self.weights,self.gradient_weights)
+        self.gradient_weights
+        if (self._optimizer!=None):
+            self.weights = self._optimizer.calculate_update(self.weights,self.gradient_weights)
         return prev
 
-    def calculate_update(self,weight_tensor,gradient_tensor):
+    #def calculate_update(self,weight_tensor,gradient_tensor):
         #self._optimizer = opt(1)
         #self._optimizer(1)
-        updated_weight = self._optimizer.calculate_update(weight_tensor,gradient_tensor)
-        return updated_weight
-
-
+        #updated_weight = self._optimizer.calculate_update(weight_tensor,gradient_tensor)
+        #return updated_weight
