a6f465ded71750fe2e970805f29f24e64529322d
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-05 10:28:33 +0200

    passed all unit tests

--- ex1/Layers/SoftMax.py
+++ ex1/Layers/SoftMax.py
@@ -6,17 +6,31 @@ class SoftMax(BayesLayer):
 
     def forward(self,input_tensor):
         self.input_tensor = input_tensor
-        input_max = np.amax(input_tensor,axis=1,keepdims=True)
+        #input_max = np.amax(input_tensor,axis=1,keepdims=True)
+        input_max=np.amax(input_tensor)
+        #print(input_max)
         reduced_exp = np.exp(input_tensor-input_max)
         reduced_sum = np.sum(reduced_exp,axis=1,keepdims=True)
         self.output_tensor = reduced_exp/reduced_sum
+
         return self.output_tensor
 
     def backward(self, error_tensor):
-        scalar =((error_tensor.flatten())*(self.output_tensor.flatten())).sum()
-        #print((error_tensor.flatten())*(self.output_tensor.flatten()))
-        #print(type(error_tensor))
-        self.prev=np.reshape((self.output_tensor.flatten())*(error_tensor.flatten()-scalar),(len(error_tensor),len(error_tensor[0])))
-        # compute gradient w.r.t weight
-        self.gradient_weights = np.matmul(self.input_tensor.T, error_tensor)
+        batch_sum=[]
+        for i in range(len(error_tensor)):
+            temp=0
+            for j in range(len(error_tensor[i])):
+                temp+= error_tensor[i][j]*self.output_tensor[i][j]
+            batch_sum.append(temp)
+
+        self.prev=[]
+        for i in range(len(error_tensor)):
+            error_tensor[i]=error_tensor[i]-batch_sum[i]
+
+        for i in range(len(error_tensor)):
+            temp=[]
+            for j in range(len(error_tensor[0])):
+                temp.append(self.output_tensor[i][j]*error_tensor[i][j])
+            self.prev.append(temp)
+        self.prev=np.reshape(self.prev,(len(error_tensor),len(error_tensor[0])))
         return self.prev
