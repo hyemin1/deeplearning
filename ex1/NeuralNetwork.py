d6f48b8891b18eea45488a73ba25eb11fc9c3908
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-06 09:20:27 +0200

    passed all tests

--- ex1/NeuralNetwork.py
+++ ex1/NeuralNetwork.py
@@ -1,40 +1,39 @@
-from Loss import CrossEntropyLoss
-from FullyConnected import FullyConnected
+from Optimization import Optimizers
 import copy
-
-class NeuralNetwork(CrossEntropyLoss, FullyConnected):
-    def __init__(self):
-        self.optimizer = None
-        self.loss = []
-        self.layers = []
-        self.data_layer = None
-        self.loss_layer = None
-
-        self.input_tensor = None
-        self.label_tensor = None
+class NeuralNetwork():
+    def __init__(self,sgd):
+        self.optimizer=sgd
+        self.loss=[]#contains loss values after calling train()
+        self.layers=[] #contains all layers
+        self.data_layer=None #contains input data & label
+        self.loss_layer=None # contain cross entropy layer
 
     def forward(self):
         self.input_tensor, self.label_tensor = self.data_layer.next()
-        last_layer_output = self.forward(self.input_tensor, self.label_tensor)
-        return last_layer_output
+        out=self.input_tensor
+        for lay in self.layers:
+            out=lay.forward(out)
+        out=self.loss_layer.forward(out,self.label_tensor)
 
+        return out
     def backward(self):
-        self.loss_layer = self.backward(self.label_tensor)
-        return self.loss
+        error = self.loss_layer.backward(self.label_tensor)
+        for lay in reversed(self.layers):
+            error = lay.backward(error)
 
-    def append_layer(self, layer):
-        fully_connected = FullyConnected()
-        if (self.trainable):
-            self.optimizer = copy.deepcopy(fully_connected.optimizer)
-            self.layers.append(layer)
+    def append_layer(self,layer):
+        if layer.trainable==True:
+            layer._optimizer =copy.deepcopy(self.optimizer)
+        self.layers.append(layer)
 
-    def train(self, iterations):
-        
+    def train(self,iterations):
+        for i in range(iterations):
+            out=self.forward()
+            self.loss.append(out)
+            self.backward()
 
-    def test(self, input_tensor):
-        fully_connected = FullyConnected()
-        fully_connected.forward(input_tensor)
-
-        softmax = SoftMax()
-        output = softmax.forward(input_tensor)
-        return output
+    def test(self,input_tensor):
+        out = input_tensor
+        for lay in self.layers:
+            out = lay.forward(out)
+        return out
