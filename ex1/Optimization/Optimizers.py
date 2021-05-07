bc1e2c174f9507eed8b7e30b7cc0048833a1edfe
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-07 23:37:02 +0200

    final

--- ex1/Optimization/Optimizers.py
+++ ex1/Optimization/Optimizers.py
@@ -1,10 +1,11 @@
+
 class Sgd():
     def __init__(self,learning_rate):
         self.learning_rate=learning_rate
 
 
     def calculate_update(self,weight_tensor,gradient_tensor):
-
+        #update weights: W - (learning rate)*X^T*E
         self.updated_weight = weight_tensor - (self.learning_rate)*(gradient_tensor)
 
         return self.updated_weight
