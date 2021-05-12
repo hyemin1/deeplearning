f4326d1265b062ec6f235f68ddbeb5f761c279d9
pritomBrainStation <50450569+pritomBrainStation@users.noreply.github.com>
2021-05-12 21:24:19 +0600

    Removed loops for ReLU

--- ex1/Layers/ReLU.py
+++ ex1/Layers/ReLU.py
@@ -25,20 +25,5 @@ class ReLU(Base.BaseLayer):
         return output
     def backward(self,error_tensor):
         #get row, col for reshaping later
-        row = len(error_tensor)
-        col = len(error_tensor[0])
-
-        self.input_tensor=self.input_tensor.flatten()
-        #make error_tensor 1D
-        error_tensor=error_tensor.flatten()
-        #initialize prev_error with 0s
-        self.prev_error=[0]*len(error_tensor)
-        #apply ReLU function for backward computation
-        #if x<=0:0, else:en
-        for i in range(len(self.input_tensor)):
-            if (self.input_tensor[i]>0):
-                self.prev_error[i]=error_tensor[i]
-        #reshape previous error tensor
-        self.prev_error=np.reshape(self.prev_error,(row,col))
-
-        return self.prev_error
+        error_tensor[self.input_tensor <= 0] = 0
+        return error_tensor
