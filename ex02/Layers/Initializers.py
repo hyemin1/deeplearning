a5d416e5b9ef1af72d2f101c952302ae2fd11dad
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-18 11:20:24 +0200

    class Constant corrected

--- ex02/Layers/Initializers.py
+++ ex02/Layers/Initializers.py
@@ -7,16 +7,16 @@ class Constant:
     def initialize(self, weights_shape, fan_in, fan_out):
         self.fan_in=fan_in
         self.fan_out=fan_out
-        if (len(weights_shape) == 2):
-            no_rows, no_columns = weights_shape
-            weights = np.full((no_rows*no_columns),self.weight_constant)
-            final_weights = np.reshape(weights, (no_rows, no_columns))
-
-        else:
-            no_rows = weights_shape[0]
-            weights = np.array(self.weight_constant,no_rows)
-            final_weights = weights
-        return final_weights
+        # if (len(weights_shape) == 2):
+        #     no_rows, no_columns = weights_shape
+        #     weights = np.full((no_rows*no_columns),self.weight_constant)
+        #     final_weights = np.reshape(weights, (no_rows, no_columns))
+        #
+        # else:
+        #     no_rows = weights_shape[0]
+        #     weights = np.array(self.weight_constant,no_rows)
+        #     final_weights = weights
+        pass
 
 class UniformRandom:
     def __init__(self):
