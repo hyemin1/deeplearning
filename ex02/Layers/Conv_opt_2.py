962ddc87ffb665c5943d8aad5ef403f9f889b3f2
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-19 22:04:11 +0200

    passed all forward related unit tests

--- ex02/Layers/Conv_opt_2.py
+++ ex02/Layers/Conv_opt_2.py
@@ -108,27 +108,21 @@ class Conv(Base.BaseLayer):
         1. calculate the output tensor shape
         2. do zero padding
         """
-        if(self.img_2d==True):
-            out_height = np.ceil(float(y) / float(self.stride_shape[0]))
-            out_width = np.ceil(float(x) / float(self.stride_shape[1]))
-            self.output_tensor = np.zeros((b,self.num_kernels,int(out_height),int(out_width)))
-            #self.output_tensor=np.zeros((b,self.num_kernels,y,x))
+        if (self.img_2d == True):
+            # width of matrix
+            x = input_tensor.shape[3]
 
-            # for batch in range(b):
-            #     for out_channel in range(self.num_kernels):
-            #         #for h in range(int(out_height)):
-            #          #   for w in range(int(out_width)):
-            #          #       subset = padded_input[batch,:,h*self.stride_shape[0]:h*self.stride_shape[0]+self.m,w*self.stride_shape[1]:w*self.stride_shape[1]+self.n]
-            #                 #self.output_tensor[batch,out_channel,h,w]=np.sum(subset*self.weights[out_channel,:,:,:]+self.bias[out_channel])
-
+        """
+        1. calculate the output tensor shape
+        2. do zero padding
+        """
+        if (self.img_2d == True):
+            self.output_tensor=np.zeros((b,self.num_kernels,y,x))
             for batch in range(b):
                 for out_channel in range(self.num_kernels):
-                    temp= signal.correlate(self.input_tensor[batch],self.weights[out_channel],mode='same')
-
-                    pad_height = (self.stride_shape[0] * temp.shape[1] - self.stride_shape[0] + self.m - temp.shape[1])
-                    pad_height = int(pad_height)
-                    pad_width = (self.stride_shape[1] * temp.shape[2] - self.stride_shape[1] + self.n - temp.shape[2])
-                    pad_width = int(pad_width)
+                    temp=self.input_tensor[batch]
+                    pad_height=self.m-1
+                    pad_width=self.n-1
 
                     if (pad_height % 2 == 0):
                         pad_top = pad_height / 2
@@ -143,42 +137,38 @@ class Conv(Base.BaseLayer):
                         pad_left = np.floor(pad_width / 2)
                         pad_right = pad_left + 1
 
-                    temp2=np.zeros((self.input_cha_num,int(temp.shape[1]+pad_height),int(temp.shape[2]+pad_width)))
+                    temp2 = np.zeros(
+                        (self.input_cha_num, int(temp.shape[1] + pad_height), int(temp.shape[2] + pad_width)))
                     for channel in range(temp.shape[0]):
-                        temp2[channel]=np.pad(temp[channel],[(int(pad_top),int(pad_bottom)),(int(pad_left),int(pad_right))],mode='constant')
-                    self.output_tensor[batch,out_channel]=signal.correlate(temp2,self.weights[out_channel],mode='valid')[0]+self.bias[out_channel]
+                        temp2[channel] = np.pad(temp[channel],
+                                                [(int(pad_top), int(pad_bottom)), (int(pad_left), int(pad_right))],
+                                                mode='constant')
+                    self.output_tensor[batch, out_channel] = signal.correlate(temp2, self.weights[out_channel], mode='valid')[0] + self.bias[out_channel]
 
-                    #print(signal.correlate(temp2,self.weights[out_channel],mode='valid')[0].shape)
-
-
-
-
-
-
+            self.output_tensor=self.output_tensor[:,:,::self.stride_shape[0],::self.stride_shape[1]]
 
         else:
-            pad_height = np.ceil((self.stride_shape[0] * y - self.stride_shape[0] + self.m - y) )
-            out_height = np.ceil(float(y) / float(self.stride_shape[0]))
-            self.output_tensor = np.zeros((b, self.num_kernels, int(out_height)))
-            if (pad_height % 2 == 0):
-                pad_top = pad_height / 2
-                pad_bottom = pad_top
-            else:
-                pad_top = np.floor(pad_height / 2)
-                pad_bottom = pad_top + 1
-            padded_input = np.zeros((b, self.c, int(y + pad_height)))
+            self.output_tensor = np.zeros((b, self.num_kernels, y))
             for batch in range(b):
-                for channel in range(self.c):
-                    padded_input[batch,channel]=np.pad(input_tensor[batch,channel],[(int(pad_top),int(pad_bottom))],mode='constant')
+                for out_channel in range(self.num_kernels):
+                    temp=self.input_tensor[batch]
+                    pad_height = self.m - 1
+                    if (pad_height % 2 == 0):
+                        pad_top = pad_height / 2
+                        pad_bottom = pad_height - pad_top
+                    else:
+                        pad_top = np.floor(pad_height / 2)
+                        pad_bottom = pad_top + 1
+                    temp2 = np.zeros(
+                        (self.input_cha_num, int(temp.shape[1] + pad_height)))
+                    for channel in range(temp.shape[0]):
+                        temp2[channel] = np.pad(temp[channel],
+                                                [(int(pad_top), int(pad_bottom))],
+                                                mode='constant')
+            self.output_tensor[batch, out_channel] = signal.correlate(temp2, self.weights[out_channel], mode='valid')[
+                                                         0] + self.bias[out_channel]
 
-            # for batch in range(b):
-            #     for out_channel in range(self.num_kernels):
-            #         for h in range(int(out_height)):
-            #             subset = padded_input[batch,:,h*self.stride_shape[0]:h*self.stride_shape[0]+self.m]
-            #             self.output_tensor[batch,out_channel,h]=np.sum(subset*self.weights[out_channel,:,:])+self.bias[out_channel]
-
-
-
+            self.output_tensor = self.output_tensor[:, :, ::self.stride_shape[0]]
         return self.output_tensor
 
 
