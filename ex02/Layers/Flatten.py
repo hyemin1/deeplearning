1bb895846f82cc2cd147bc10fd674f99150f12c3
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-05-29 22:21:23 +0200

    changed shaping

--- ex02/Layers/Flatten.py
+++ ex02/Layers/Flatten.py
@@ -3,17 +3,31 @@ import numpy as np
 class Flatten(Base.BaseLayer):
     def __init__(self):
         super().__init__()
+        self.batch=0
+        self.ch=0
+        self.height=0
+        self.width=0
 
     def forward(self,input_tensor):
-        self.a=len(input_tensor)
-        self.b = len(input_tensor[0])
-        self.c = len(input_tensor[0][0])
-        self.d=len(input_tensor[0][0][0])
+        #store the shape of input tensor
+        self.batch=len(input_tensor)
+        self.ch = len(input_tensor[0])
+        self.height = len(input_tensor[0][0])
+        if(self.height!=0):
+         self.width=len(input_tensor[0][0][0])
+
         self.input_flatt = []
-        for i in range(len(input_tensor)):
+        #flat matrix for all batches
+        for i in range(self.batch):
             temp=input_tensor[i].flatten()
             self.input_flatt.append(temp)
         return self.input_flatt
     def backward(self,error_tensor):
-        self.error_flatt = np.reshape(error_tensor,(self.a,self.b,self.c,self.d))
-        return self.error_flatt
+        #reshape error tensor
+        #to 4D
+        if(self.width!=0):
+            self.error_unflatt = np.reshape(error_tensor,(self.batch,self.ch,self.height,self.width))
+        #to 3D
+        else:
+            self.error_unflatt=np.reshape(error_tensor,(self.batch,self.ch,self.height))
+        return self.error_unflatt
