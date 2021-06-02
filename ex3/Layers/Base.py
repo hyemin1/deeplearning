7bc27fdf9a4b45f9440d46766c3a97d6f948d6e8
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-06-02 20:41:19 +0200

    added testing_phase

---
+++ ex3/Layers/Base.py
@@ -0,0 +1,8 @@
+import numpy as np
+class BaseLayer():
+    def __init__(self):
+        self.trainable=False
+        self.weights=np.empty((1,1))
+        self.input_size=0
+        self.output_size=0
+        self.testing_phase=False
