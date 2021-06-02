e0a033ef1819373b83efa6fbe3006a3498b42d10
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-06-02 16:56:09 +0200

    passed all unit tests

---
+++ ex3/Optimization/Constraints.py
@@ -0,0 +1,16 @@
+import numpy as np
+class L2_Regularizer:
+    def __init__(self,alpha):
+        self.alpha = alpha #lambda of the norm
+    def calculate_gradient(self,weights):
+        return self.alpha*weights
+    def norm(self,weights):
+        return self.alpha*np.square(np.linalg.norm(weights,'fro'))
+
+class L1_Regularizer:
+    def __init__(self,alpha):
+        self.alpha =alpha
+    def calculate_gradient(self,weights):
+        return self.alpha*np.sign(weights)
+    def norm(self,weights):
+        return self.alpha*np.sum(np.abs(weights))
