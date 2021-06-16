828253d7402b1c139287f2aea1c86d3b3ebb9bab
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-06-16 17:02:34 +0200

    updated reg

--- ex3/Layers/Conv.py
+++ ex3/Layers/Conv.py
@@ -12,7 +12,7 @@ class Conv(Base.BaseLayer):
         self.stride_shape = stride_shape
         self.convolution_shape = convolution_shape
         self.num_kernels = num_kernels
-        self.n=True
+        self.reg=True
         self.opt_w = None
         self.opt_b = None
 
