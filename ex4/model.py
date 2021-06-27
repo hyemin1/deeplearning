5d61b9c0b131ec564d585a822e852e8e5a0d76c5
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-06-27 08:23:53 +0200

    passed all unit tests

--- ex4/model.py
+++ ex4/model.py
@@ -1,38 +1,89 @@
 import torch
-class ResBlock(torch.nn.Module):
-    def __init__(self,in_channels,out_channels,stride):
-        super().__init__()
-        self.cv_1=torch.nn.Conv2d(in_channels, out_channels, kernel_size=3,stride=stride)
-        self.cv_2=torch.nn.Conv2d(in_channels, out_channels, kernel_size=3,stride=1)
-        self.batch_norm_1=torch.nn.BatchNorm2d(out_channels)
-        self.batch_norm_2 = torch.nn.BatchNorm2d(out_channels)
-        self.relu_1=torch.nn.ReLU(inplace=True)
-        self.relu_2 = torch.nn.ReLU(inplace=True)
+# Residual block
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, stride=stride,padding=1,bias=False,kernel_size=3)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels,kernel_size=3,padding=1,bias=False,stride=1)
+        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
 
-        self.cv_3 =torch.nn.Conv2d(in_channels,out_channels,kernel_size=1,stride=stride)
-        self.batch_norm_3 = torch.nn.BatchNorm2d(out_channels)
-    def forward(self,input_tensor):
-        output=self.cv_1(input_tensor)
-        output=self.batch_norm_1(output)
-        output=self.relu_1(output)
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
 
-        output=self.cv_2(output)
-        output=self.batch_norm_2(output)
-        # output +=input_tensor
-        output=self.relu_2(output)
 
-        extra = self.cv_3(input_tensor)
-        extra = self.batch_norm_3(extra)
-        output+=extra
-        return output
+# ResNet
 class ResNet(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lay_1=torch.nn.Sequential(torch.nn.Conv2d(3,64,7,2),torch.nn.BatchNorm2d(),torch.torch.nn.ReLU(),torch.nn.MaxPool2d(3,2))
-        self.lay_2=torch.nn.Sequential(ResBlock(64,64,1),ResBlock(64,128,2),ResBlock(128,256,2),ResBlock(256,512,2))
-        self.lay_3=torch.nn.Sequential(torch.nn.AvgPool2d(),torch.nn.flatten(),torch.nn.Linear(512,2),torch.nn.Sigmoid())
-    def forward(self,input_tensor):
-        output=self.lay_1(input_tensor)
-        output=self.lay_2(output)
-        output=self.lay_3(output)
-        return output
+    def __init__(self,num_classes=10):
+        layers=2
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv = torch.nn.Conv2d(3, 64,kernel_size=7,stride=2,padding=1,bias=False)
+        self.bn = torch.nn.BatchNorm2d(64)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.mpool=torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self.make_layer(ResidualBlock, 64, layers)
+        self.layer2 = self.make_layer(ResidualBlock, 128, layers, 2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, layers, 2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, layers, 2)
+        self.avg_pool = torch.nn.AdaptiveAvgPool2d((1,1))
+        self.fc = torch.nn.Linear(512, 2)
+        self.sig=torch.nn.Sigmoid()
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = torch.nn.Sequential(
+                torch.nn.Conv2d(self.in_channels, out_channels, stride=stride,kernel_size=3,padding=1,bias=False),
+                torch.nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out=self.mpool(out)
+        out = self.layer1(out)
+        print(out.shape)
+        out = self.layer2(out)
+        print(out.shape)
+        out = self.layer3(out)
+        print(out.shape)
+        out=self.layer4(out)
+        print(out.shape)
+        # out = out.view(out.size(0), -1)
+        # print(out.shape)
+        out = self.avg_pool(out)
+        print(out.shape)
+        # out=torch.flatten(out)
+        # print(out.shape)
+        # # out = out.view(out.size(0), -1)
+        # print(out.shape)
+        # out=torch.flatten(out)
+        # print(out.shape)
+        out= out.view(out.size(0), -1)
+        print(out.shape)
+        out = self.fc(out)
+        print(out)
+        out=self.sig(out)
+        print(out)
+
+
+        return out
