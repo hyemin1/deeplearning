47b5d993b09174a2b629a4c875d3beb514a3aafa
hyemin1 <52363215+hyemin1@users.noreply.github.com>
2021-07-06 10:04:44 +0200

    new hyper parameters

--- ex4/train.py
+++ ex4/train.py
@@ -10,16 +10,16 @@ from sklearn.model_selection import train_test_split
 """
 hyper parameters
 """
-batch_size=10
-learning_rate=0.001
-total_epoch=15
+batch_size=30
+learning_rate=0.00003
+total_epoch=200
 
 
 # load the data from the csv file and perform a train-test-split
 data = pd.read_csv("data.csv",sep=';')
 
 # this can be accomplished using the already imported pandas and sklearn.model_selection modules
-train,test = train_test_split(data,test_size=0.3)
+train,test = train_test_split(data,test_size=0.2)
 
 
 # set up data loading for the training and validation set each using t.utils.data.DataLoader and ChallengeDataset objects
@@ -43,10 +43,11 @@ loss=t.nn.BCELoss()
 """
 choose optimizer
 """
-opt=t.optim.SGD(resnet.parameters(),lr=learning_rate)
-
+# opt=t.optim.SGD(resnet.parameters(),lr=learning_rate)
+# opt=t.optim.SGD(resnet.parameters(),lr=learning_rate,momentum=0.9)
+opt=t.optim.Adam(resnet.parameters(),lr=learning_rate)
 # create an object of type Trainer and set its early stopping criterion
-trainer=Trainer(early_stopping_patience=2,model=resnet,crit=loss,optim=opt,train_dl=train_loader,val_test_dl=val_loader,cuda="cuda")
+trainer=Trainer(early_stopping_patience=10,model=resnet,crit=loss,optim=opt,train_dl=train_loader,val_test_dl=val_loader,cuda=True)
 res=[[0],[0]]
 # go, go, go... call fit on trainer
 res[0],res[1]=trainer.fit(total_epoch)
@@ -57,3 +58,7 @@ plt.plot(np.arange(len(res[1])), res[1], label='val loss')
 plt.yscale('log')
 plt.legend()
 plt.savefig('losses.png')
+
+# for epoch in range(0,total_epoch%5):
+#     trainer.restore_checkpoint(epoch)
+#     trainer.save_onnx('checkpoint_{:03d}.onnx'.format(epoch))
